7ce607ac32387d73de79ebbd2b19bfc811b3f6db
baddabudda <radkova.irene@gmail.com>
2025-04-16 19:55:36 +0300

    feat: add models

--- model/__init__.py
+++ model/__init__.py
@@ -1,7 +1,22 @@
 import numpy as np
 
+def to_np_array(list) -> np.array:
+    types = [('id', int), ('price', int), ('weight', int)]
+    return np.array(list, dtype=types)
+
 class Problem:
     def __init__(self, total_capacity, items):
-        types = [('id', int), ('price', int), ('weight', int)]
         self.total_capacity = total_capacity
-        self.items = np.array(items, dtype=types)
+        self.items = to_np_array(items)
+    
+    def get_item_price(self, id):
+        return self.items[id][1]
+    
+    def get_item_weight(self, id):
+        return self.items[id][2]
+
+class Result:
+    def __init__(self, occupied_capacity, total_price, items_taken):
+        self.occupied_capacity = occupied_capacity
+        self.total_price = total_price
+        self.items_taken = to_np_array(items_taken)
