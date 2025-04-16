dacd7cc687a2872930fe62007a7cfd2b76a7b1d2
baddabudda <radkova.irene@gmail.com>
2025-04-16 20:29:46 +0300

    refactor: rename to profit

--- model/__init__.py
+++ model/__init__.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def to_np_array(list) -> np.array:
-    types = [('id', int), ('price', int), ('weight', int)]
+    types = [('id', int), ('profit', int), ('weight', int)]
     return np.array(list, dtype=types)
 
 class Problem:
@@ -9,14 +9,14 @@ class Problem:
         self.total_capacity = total_capacity
         self.items = to_np_array(items)
     
-    def get_item_price(self, id):
+    def get_item_profit(self, id):
         return self.items[id][1]
     
     def get_item_weight(self, id):
         return self.items[id][2]
 
 class Result:
-    def __init__(self, occupied_capacity, total_price, items_taken):
+    def __init__(self, occupied_capacity, total_profit, items_taken):
         self.occupied_capacity = occupied_capacity
-        self.total_price = total_price
+        self.total_profit = total_profit
         self.items_taken = to_np_array(items_taken)
